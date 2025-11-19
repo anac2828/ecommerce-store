9cd564f032ad9f48964678281339b8abb934daba
Ana <ana@Anas-MacBook-Air.local>
2025-11-18 22:43:52 -0800

    created products are now displayed on products page

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,6 +1,7 @@
 import { PageHeader } from '../../_components/PageHeader';
 import { ProductForm } from './_components/ProductForm';
 
+// Page for adding a new product in the admin dashboard
 export default function NewProductPage() {
   return (
     <>
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link';
+import { CheckCircle2, MoreVertical, XCircle } from 'lucide-react';
+import db from '@/db/db';
+import { formatCurrency, formatNumber } from '@/lib/formaters';
 import { Button } from '@/components/ui/button';
 import { PageHeader } from '../_components/PageHeader';
-import Link from 'next/link';
-import { Table, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Table, TableHead, TableHeader, TableRow, TableBody, TableCell } from '@/components/ui/table';
+import { DropdownMenu, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import { DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu';
 
+// List of products for the admin dashboard
 export default function AdminProductPage() {
   return (
     <>
@@ -18,9 +24,25 @@ export default function AdminProductPage() {
   );
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      // Number of orders for the product
+      _count: {
+        select: { orders: true }
+      } 
+    }
+  })
+
+  if(products.length === 0) return <p>No products found.</p>
+  
   return (
     <Table>
+      {/* HEADER */}
       <TableHeader>
         <TableRow>
           <TableHead className='w-0'>
@@ -34,6 +56,44 @@ function ProductsTable() {
           </TableHead>
         </TableRow>
       </TableHeader>
+      <TableBody>
+        {products.map(product => 
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ?
+              <>
+              <span className='sr-only'>Available</span>
+              <CheckCircle2/>
+              </> :
+              <>
+              <span className='sr-only'>Unavailable</span>
+              <XCircle/>
+              </>}
+            </TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>${product.priceInCents / 100}</TableCell>
+            <TableCell>{formatNumber(product._count.orders)}</TableCell>
+            <TableCell>
+              <DropdownMenu>
+                {/* Button */}
+                <DropdownMenuTrigger>
+                    <MoreVertical />
+                    <span className='sr-only'>Actions</span>
+                  </DropdownMenuTrigger>
+                  {/* Content */}
+                  <DropdownMenuContent>
+                    <DropdownMenuItem asChild>
+                      <a href={`/admin/products/${product.id}/download`}>Download</a>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                      <Link href={`/admin/products/${product.id}/edit`}>Edit</Link>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+            </TableCell>
+            </TableRow>
+        )}
+      </TableBody>
     </Table>
   );
 }
